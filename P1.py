766f53e256973733070e08927ec7395122d93ab4
Dingjin0924 <a9609241@gmail.com>
2024-09-28 23:49:36 +0800

    second

--- P1.py
+++ P1.py
@@ -1,18 +1,22 @@
-import numpy as np
+# 數據向量化
+import numpy as np 
+
+# 建立表格，方便觀看
 import pandas as pd
+
+# 取得鳶尾花(iris)的數據集和訓練式
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelBinarizer
+
+# 取得類神經網路學習
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # 加載鳶尾花數據集
 iris = load_iris()
 
-# 呼叫 iris  的資料跟欄位名稱
+# 呼叫 iris 的資料跟欄位名稱
 iris_data = iris.data
 iris_columns = iris.feature_names
 iris_target = iris.target
@@ -24,7 +28,7 @@ df['target'] = iris_target
 #print(df)
 
 # 將數值型的標籤轉換為對應的類別名稱
-df['target'] = df['target'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
+# df['target'] = df['target'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
 
 # 查看數據集
 # print(df.head())
@@ -46,22 +50,24 @@ markers 三種花的特徵資料用甚麼圖案區分  o、s、D ：分别表示
 
 
 
-# 資料預先處理
+# 資料預先處理，向量化
 # ==========================
 iris_categorical_target = to_categorical(iris_target)
 """
-
 0 -> [1. 0. 0.]
 1 -> [0. 1. 0.]
 2 -> [0. 0. 1.]
 """
 
 # 分割資料為訓練集和測試集
-X_train, X_teat, y_train, y_test = train_test_split(iris_data, iris_categorical_target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(iris_data, iris_categorical_target, test_size=0.2, random_state=42)
 
 """
 train_test_split
-
+第一個參數為特徵資料(花萼花瓣的長寬)
+第二個參數為答案(花的種類)
+第三個參數test_size 為切割測試資料的比例
+random_state = 42 原因 https://blog.csdn.net/x5445687d/article/details/120258252
 """
 
 
@@ -69,16 +75,19 @@ train_test_split
 # 構建類神經網路模型
 # ===================
 model = Sequential()
+
 # 添加輸入層和一個隱藏層，使用 ReLU 激活函數
 model.add(Dense(units=32, input_shape=(4,), activation='relu'))
 """
 輸入層就是 input_shape, 資料型態要是 tuple, (4,) 代表輸入層為4個神經元
-Dense 後面的數字就是隱藏層的神經元數量
+unit 後面的數字就是隱藏層(Dense)的神經元數量
 activation 激活函數
 """
+
 # 如要添加新的隱藏層 就不需要 input_shape
 model.add(Dense(units=16, activation='relu'))
-# 添加輸出層，使用 softmax 激活函數進行多分類
+
+# 添加輸出層，使用 softmax 激活函數進行多分類(變成概率值來算最接近哪種花的向量)
 model.add(Dense(3, activation='softmax'))
 """
 這邊輸出層神經元數量為3是因為花種只有3個
@@ -87,13 +96,15 @@ model.add(Dense(3, activation='softmax'))
 接近 [0. 1. 0.] -> 1
 接近 [0. 0. 1.] -> 2
 """
+
 # 編譯模型
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 """
-optimizer 優化器, 使用adam公式優化梯度下降計算
-loss 損失函數
+使用 optimizer 優化器中的 adam 公式優化梯度下降計算(調整偏差值、權重和學習率)
+loss 損失函數(衡量預測結果和真實結果的差距)
 metrics 參數用於指定在訓練和評估過程中需要監控的指標。metrics=['accuracy'] 表示在訓練和評估模型時， Keras 會計算並輸出模型的準確率。
 """
+
 # 顯示模型架構
 model.summary()
 # ==============================
